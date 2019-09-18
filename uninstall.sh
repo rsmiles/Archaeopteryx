4a812136fa83449f0fb5a66a485d4eb4ff0d4c52
Robert Smiley <yarnoiser@gmail.com>
2019-09-18 17:50:13 -0600

    Correct error with uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 
 deluser archaeopteryx
 rm -rf /home/archaeopteryx
-grep -v 'archaeopteryx' < /etc/cron.allow > /etc/cron.allow
+cat /etc/cron.allow | grep -v 'archaeopteryx' > /etc/cron.allow
 rm -rf /root/.Archaeopteryx
 rm -rf /root/.Trash
 
