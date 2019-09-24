4a6556a18c94e330aa4a309e7bdb2e53c1ada525
Robert Smiley <yarnoiser@gmail.com>
2019-09-24 07:22:26 -0600

    maintenance and root schedule tests

--- maintenance.sh
+++ maintenance.sh
@@ -2,64 +2,64 @@
 
 . ~/.Archaeopteryx/lib.sh
 
-KEEP_TRASH=100
-WARNING_MESG='System maintenance will begin in'
-LOG_DIR='/var/log/Archaeopteryx/'
-KEEP_LOGS=30
+#KEEP_TRASH=100
+#WARNING_MESG='System maintenance will begin in'
+#LOG_DIR='/var/log/Archaeopteryx/'
+#KEEP_LOGS=30
 
-count_files(){
-	ls $1 | wc -l
-}
+#count_files(){
+#	ls $1 | wc -l
+#}
 
-wall "$WARNING_MESG 1 hour"
+#wall "$WARNING_MESG 1 hour"
 
-sleep 30m
+#sleep 30m
 
-wall "$WARNING_MESG 30 minutes"
+#wall "$WARNING_MESG 30 minutes"
 
-sleep 20m
+#sleep 20m
 
-wall "$WARNING_MESG 10 minutes"
+#wall "$WARNING_MESG 10 minutes"
 
-sleep 5m
+#sleep 5m
 
-wall "$WARNING_MESG 5 minutes"
+#wall "$WARNING_MESG 5 minutes"
 
-sleep 4m
+#sleep 4m
 
-wall "$WARNING_MESG 1 minute"
+#wall "$WARNING_MESG 1 minute"
 
-sleep 1m
+#sleep 1m
 
-wall 'Starting system maintenance. System restart will occur at the end.'
+#wall 'Starting system maintenance. System restart will occur at the end.'
 
 # Empty trash if it has more than KEEP_TRASH items in it
-if [ $(count_files $TRASH) -ge $KEEP_TRASH ]
-then
-	rmtrash
-fi
+#if [ $(count_files $TRASH) -ge $KEEP_TRASH ]
+#then
+#	rmtrash
+#fi
 
-if [ ! -d $LOG_DIR ]
-then
-	mkdir $LOG_DIR
-	chown archaeopteryx: $LOG_DIR
-	chmod 750 $LOG_DIR
-fi
+#if [ ! -d $LOG_DIR ]
+#then
+#	mkdir $LOG_DIR
+#	chown archaeopteryx: $LOG_DIR
+#	chmod 750 $LOG_DIR
+#fi
 
 # If our number of log files is over KEEP_LOGS, then trash the oldest one
-if [ $(count_files $LOG_DIR) -gt $KEEP_LOGS ]
-then
-	trash $(ls $LOG_DIR | sort | head -n 1)
-fi
+#if [ $(count_files $LOG_DIR) -gt $KEEP_LOGS ]
+#then
+#	trash $(ls $LOG_DIR | sort | head -n 1)
+#fi
 
-TIME=$(tstmp)
-LOG_FILE="$LOG_DIR"/"$TIME".log
-touch $LOG_FILE
-chown archaeopteryx $LOG_FILE
-chmod 750 $LOG_FILE
+#TIME=$(tstmp)
+#LOG_FILE="$LOG_DIR"/"$TIME".log
+#touch $LOG_FILE
+#chown archaeopteryx $LOG_FILE
+#chmod 750 $LOG_FILE
 
-apt-get -y -qq update >>$LOG_FILE 2>>$LOG_FILE
-apt-get -y -qq upgrade >>$LOG_FILE 2>>$LOG_FILE
+#apt-get -y -qq update >>$LOG_FILE 2>>$LOG_FILE
+#apt-get -y -qq upgrade >>$LOG_FILE 2>>$LOG_FILE
 
-shutdown -r +5
+shutdown -r now
 
