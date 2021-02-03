ae266f1c030f04ec4c70decef2f420b295183a62
Robert Smiley <yarnoiser@gmail.com>
2021-02-03 13:11:35 -0700

    Removed sh components, ready for testing

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-deluser archaeopteryx
-rm -rf /home/archaeopteryx
-cat /etc/cron.allow | grep -v 'archaeopteryx' > /etc/cron.allow
-rm -rf /root/.Archaeopteryx
-rm -rf /root/.Trash
-rm -rf /var/log/Archaeopteryx
-
